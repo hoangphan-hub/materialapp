e63ae0ba449144ec75e8ae217f5447c5b1ba8f97
Hoang Phan <hoangpn285@gmail.com>
2021-09-10 10:50:53 +0700

    nesting widgets and change light

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,51 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String light = 'GREEN';
+  String instruction = 'GO';
+  Color? instructionColor = Colors.green[500];
+
+  @override
+  void initState() {
+    light = 'GREEN';
+    instruction = 'GO';
+    instructionColor = Colors.green[500];
+    super.initState();
+  }
+
+  void changeLight() {
+    setState(() {
+      if (light == 'GREEN') {
+        light = 'YELLOW';
+        instruction = 'SLOW';
+        instructionColor = Colors.orange[500];
+      } else if (light == 'YELLOW') {
+        light = 'RED';
+        instruction = 'STOP';
+        instructionColor = Colors.red[500];
+      } else {
+        light = 'GREEN';
+        instruction = 'GO';
+        instructionColor = Colors.green[500];
+      }
+    });
+  }
+
+  int turnOnLight(String lightactive) {
+    int num = 100;
+    if (light == lightactive) {
+      num = 500;
+    }
+    return num;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,39 +56,43 @@ class MyApp extends StatelessWidget {
       title: 'Material App',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Material App'),
+          title: const Text('Traffic Light'),
           backgroundColor: Colors.teal[300],
         ),
         body: Center(
-          child: Column(children: [
-            Text(
-              'Traffic Light',
-              style: TextStyle(
-                fontSize: 32,
-                color: Colors.teal[500],
-                fontWeight: FontWeight.bold,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                instruction,
+                style: TextStyle(fontSize: 32, color: instructionColor),
               ),
-            ),
-            Row(
-              children: [
-                Icon(
-                  Icons.circle,
-                  size: 100,
-                  color: Colors.green[500],
-                ),
-                Icon(
-                  Icons.circle,
-                  size: 100,
-                  color: Colors.yellow[500],
-                ),
-                Icon(
-                  Icons.circle,
-                  size: 100,
-                  color: Colors.red[500],
-                ),
-              ],
-            )
-          ]),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.circle,
+                    size: 100,
+                    color: Colors.green[turnOnLight('GREEN')],
+                  ),
+                  Icon(
+                    Icons.circle,
+                    size: 100,
+                    color: Colors.orange[turnOnLight('YELLOW')],
+                  ),
+                  Icon(
+                    Icons.circle,
+                    size: 100,
+                    color: Colors.red[turnOnLight('RED')],
+                  ),
+                ],
+              ),
+              ElevatedButton(
+                onPressed: changeLight,
+                child: Text('Next', style: TextStyle(fontSize: 20),),
+              ),
+            ],
+          ),
         ),
       ),
     );
